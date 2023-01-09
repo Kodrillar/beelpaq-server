3d926b9d308646819a7f70e5861fa0f00e0fb8ce
Kodrillar <ipadeoladavid112@gmail.com>
2023-01-09 22:07:57 +0100

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import appRouter from './routes/app-route';
+import  cors from 'cors';
+
+
 
 const app = express();
-
+app.set('trust proxy', true)
+app.use(cors());
 app.use('/api/v1', appRouter)
-
+ 
 export default app;

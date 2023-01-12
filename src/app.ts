0f757e459aa6f42dd7a794713a7feb087550b386
Kodrillar <ipadeoladavid112@gmail.com>
2023-01-12 18:17:23 +0100

    secure http headers

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import appRouter from './routes/app-route';
 import  cors from 'cors';
+import helmet from 'helmet';
 
 
 
 const app = express();
-app.set('trust proxy', true)
 app.use(cors());
+app.use(helmet());
 app.use('/api/v1', appRouter)
  
 export default app;

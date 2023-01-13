051e846eea1d0be3d93d7f831d03433822154d0f
Kodrillar <ipadeoladavid112@gmail.com>
2023-01-13 14:15:43 +0100

    auth implementation

--- src/routes/index/index.controller.ts
+++ src/routes/index/index.controller.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 
-
 export default function indexMessageController  (req:Request, res:Response){
     res.json({message:"Welcome to Beelpaq API V1"})
 }
